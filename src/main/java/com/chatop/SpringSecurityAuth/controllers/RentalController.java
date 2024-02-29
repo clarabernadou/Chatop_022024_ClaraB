ed5bb8f58669b4bf802092c736b839d838e918a2
clarabernadou <clarabernadou56690@gmail.com>
2024-02-29 23:23:59 +0100

    [add] - add Swagger to rental files

--- src/main/java/com/chatop/SpringSecurityAuth/controllers/RentalController.java
+++ src/main/java/com/chatop/SpringSecurityAuth/controllers/RentalController.java
@@ -18,14 +18,28 @@ import com.chatop.SpringSecurityAuth.dto.RentalDTO;
 import com.chatop.SpringSecurityAuth.model.MessageResponse;
 import com.chatop.SpringSecurityAuth.services.RentalService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
+
+@Tag(name = "Rental", description = "Rental management APIs")
 public class RentalController {
     @Autowired
     private RentalService rentalService;
 
+    @Operation(summary = "Post a new Rental", tags = { "rentals", "post" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Rental created", content = @Content(mediaType = "application/json", schema = @Schema(implementation = RentalDTO.class))),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @PostMapping("/rentals")
     public ResponseEntity<?> createRental(@Valid @RequestBody RentalDTO rentalDTO, Errors errors) {
         if(errors.hasErrors()) {
@@ -35,6 +49,11 @@ public class RentalController {
         return ResponseEntity.ok(new MessageResponse(rentalService.createRental(rentalDTO).get()));
     }
 
+    @Operation(summary = "Get all Rentals", tags = { "rentals", "get" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "List of Rentals", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = RentalDTO.class)))),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @GetMapping("/rentals")
     public ResponseEntity<List<RentalDTO>> getRentals() {
         List<RentalDTO> rentals = rentalService.getRentals();
@@ -46,6 +65,11 @@ public class RentalController {
         return ResponseEntity.ok(rentals);
     }
 
+    @Operation(summary = "Get a Rental by ID", tags = { "rentals", "get" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Rental found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = RentalDTO.class))),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @GetMapping("/rentals/{id}")
     public ResponseEntity<?> getRental(@PathVariable Long id) {
         if(rentalService.getRental(id) == null) {
@@ -55,6 +79,11 @@ public class RentalController {
         return ResponseEntity.ok(rentalService.getRental(id));
     }
 
+    @Operation(summary = "Update a Rental by ID", tags = { "rentals", "put" })
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Rental updated", content = @Content(mediaType = "application/json", schema = @Schema(implementation = RentalDTO.class))),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+    })
     @PutMapping("/rentals/{id}")
     public ResponseEntity<?> updateRental(@PathVariable Long id, @Valid @RequestBody RentalDTO rentalDTO, Errors errors) {
         if(rentalService.getRental(id) == null) {
--- src/main/java/com/chatop/SpringSecurityAuth/dto/RentalDTO.java
+++ src/main/java/com/chatop/SpringSecurityAuth/dto/RentalDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -14,23 +15,30 @@ public class RentalDTO {
     private Long id;
 
     @NotEmpty
+    @Schema(description = "Rental's name", example = "dream house")
     private String name;
 
     @NotNull
+    @Schema(description = "Rental's surface", example = "400")
     private Integer surface;
 
     @NotNull
+    @Schema(description = "Rental's price", example = "1200")
     private Integer price;
 
     @NotEmpty
+    @Schema(description = "Rental's description", example = "This is a description.")
     private String description;
 
     @NotNull
+    @Schema(description = "Rental's owner id", example = "1")
     private Long ownerId;
 
     @JsonProperty("created_at")
+    @Schema(description = "Rental's creation date", example = "2021-09-01")
     private LocalDate createdAt;
 
     @JsonProperty("updated_at")
+    @Schema(description = "Rental's update date", example = "2021-09-02")
     private LocalDate updatedAt;
 }
