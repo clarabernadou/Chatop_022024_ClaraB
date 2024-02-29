e362d7ab08cbede4f0f1c7bf05a4d8396b75fc2b
clarabernadou <clarabernadou56690@gmail.com>
2024-02-29 23:22:39 +0100

    [add] - add Swagger to user file

--- src/main/java/com/chatop/SpringSecurityAuth/dto/UserDTO.java
+++ src/main/java/com/chatop/SpringSecurityAuth/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.chatop.SpringSecurityAuth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
@@ -7,11 +8,14 @@ import lombok.Data;
 public class UserDTO {
 
     @NotEmpty
+    @Schema(description = "User's id", example = "1")
     private Long id;
 
     @NotEmpty
+    @Schema(description = "User's name", example = "Michel")
     private String name;
 
     @NotEmpty
+    @Schema(description = "User's email", example = "test@test.fr")
     private String email;
 }
