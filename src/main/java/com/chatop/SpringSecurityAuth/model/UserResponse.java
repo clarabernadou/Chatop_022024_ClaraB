39ecfa3484062dfe9a3cce3a97c319bbe29e0153
clarabernadou <clarabernadou56690@gmail.com>
2024-02-29 23:23:21 +0100

    [add] - add Swagger to user response file

--- src/main/java/com/chatop/SpringSecurityAuth/model/UserResponse.java
+++ src/main/java/com/chatop/SpringSecurityAuth/model/UserResponse.java
@@ -2,12 +2,12 @@ package com.chatop.SpringSecurityAuth.model;
 
 import com.chatop.SpringSecurityAuth.dto.UserDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "User Model Information")
 public class UserResponse extends UserDTO {
-    
     private String createdAt;
-
     private String updatedAt;
 }
