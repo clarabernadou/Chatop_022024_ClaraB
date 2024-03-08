a59f90b096457ea593c9a27f67f8a2aa7abd0644
clarabernadou <clarabernadou56690@gmail.com>
2024-03-08 14:25:03 +0100

    [add] - add path in spring config

--- src/main/java/com/chatop/SpringSecurityAuth/configuration/SpringSecurityConfig.java
+++ src/main/java/com/chatop/SpringSecurityAuth/configuration/SpringSecurityConfig.java
@@ -31,7 +31,7 @@ public class SpringSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/auth/register", "/api/auth/login","/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        auth.requestMatchers("/api/auth/register", "/api/auth/login","/swagger-ui/**", "/v3/api-docs/**", "/api/rentals/images/**").permitAll()
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
