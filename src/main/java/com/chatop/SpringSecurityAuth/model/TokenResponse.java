ce1ea16f942312acf9a2aa9b66229f6b69a964cc
clarabernadou <clarabernadou56690@gmail.com>
2024-03-09 17:11:02 +0100

    [add] - add Swagger in token response

--- src/main/java/com/chatop/SpringSecurityAuth/model/TokenResponse.java
+++ src/main/java/com/chatop/SpringSecurityAuth/model/TokenResponse.java
@@ -1,10 +1,12 @@
 package com.chatop.SpringSecurityAuth.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class TokenResponse {
+    @Schema(description = "Token", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0b3RvQHRlc3QuZnIifQ.3")
     private String token;
 }
