ce0aa5be5433dcde510ecaa36b3d7b00f9aa426c
clarabernadou <clarabernadou56690@gmail.com>
2024-03-10 17:36:13 +0100

    [add] - add Swagger to Rental

--- src/main/java/com/chatop/SpringSecurityAuth/dto/RentalPictureDTO.java
+++ src/main/java/com/chatop/SpringSecurityAuth/dto/RentalPictureDTO.java
@@ -2,9 +2,12 @@ package com.chatop.SpringSecurityAuth.dto;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class RentalPictureDTO extends RentalDTO {
+    
+    @Schema(description = "Rental's picture", example = "image.JPEG")
     private MultipartFile picture;
 }
