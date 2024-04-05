d4c1aa945a2764e68bc6522ff73151554ef8a78c
clarabernadou <clarabernadou56690@gmail.com>
2024-04-05 15:28:28 +0200

    [del] - delete name in auth dto + hidden id

--- src/main/java/com/chatop/SpringSecurityAuth/dto/AuthDTO.java
+++ src/main/java/com/chatop/SpringSecurityAuth/dto/AuthDTO.java
@@ -1,5 +1,6 @@
 package com.chatop.SpringSecurityAuth.dto;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
@@ -7,16 +8,13 @@ import lombok.Data;
 @Data
 public class AuthDTO {
 
+    @Hidden
     @Schema(description = "Auth's id", example = "1")
     private Long id;
 
     @Schema(description = "Auth's email", example = "toto@test.fr")
     @NotEmpty
     private String email;
-
-    @Schema(description = "Auth's name", example = "toto")
-    @NotEmpty
-    private String name;
 
     @Schema(description = "Auth's password", example = "lemotdepassedetoto")
     @NotEmpty
